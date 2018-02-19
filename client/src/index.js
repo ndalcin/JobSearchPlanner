3a32793e731ba8d8aa730be8c58fd262610d21b3
ndalcin <dalcin.nicole@gmail.com>
2018-02-19 09:02:08 -0500

    installed dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import RootReducer from './reducers/index';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  RootReducer,
+  compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+class Hello extends React.Component {
+  render() {return <div>Hello!</div>}
+}
+class Goodbye extends React.Component {
+  render() {return <div>Goodbye!</div>}
+}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
