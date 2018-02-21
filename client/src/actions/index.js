a03d4beb74fa3a7dfb1c350adb57312687896d89
ndalcin <dalcin.nicole@gmail.com>
2018-02-21 10:19:45 -0500

    create task action for onSubmit form

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,7 @@ import fetch from 'isomorphic-fetch';
 require('es6-promise').polyfill();
 export const FETCH_TASKS = 'FETCH_TASKS';
 export const RECEIVED_TASKS = 'RECEIVED_TASKS';
+export const CREATE_TASK = 'CREATE_TASK';
 const ROOT_URL = 'http://localhost:3000/api';
 
 export function fetchTasks() {
@@ -13,4 +14,18 @@ export function fetchTasks() {
         dispatch({type: RECEIVED_TASKS, payload: json})
       })
   };
+}
+
+export function createTask(values) {
+  return (dispatch) => {
+    fetch(`${ROOT_URL}/tasks`, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+        .then(json => dispatch({ type: CREATE_TASK, payload: json }))
+  }
 }
