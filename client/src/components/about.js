338b04f872c753296278a648cda5ebe83d20f97c
ndalcin <dalcin.nicole@gmail.com>
2018-02-28 09:53:48 -0500

    about section complete

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>about section</div>
+    <div className="container about-section">
+      <h2>About Job Search Task Tracker</h2>
+      <p>An exciting piece of Flatiron's Full-Stack Web Development Program is that it is a Job-Offer Guarantee Qualifying Program.
+      This means that, after completion of the program, if you do not receive a Qualifying Job Offer within 6 months, you are eligible for a full refund.
+      Read a detailed explanation about this guarantee<a href="https://learn.co/tos">here</a>.</p>
+
+      <p>While this is an exciting offer, Flatiron grads are still responsible to engage in a rigorous job search.
+      In order to remain eligible for the full tuition refund, you must meet all of the job search related requirements during the 180 day period of their job search.
+      This includes: correspondences with prospective employers, networking with members of the programming community, weekly GitHub commits and blog posts, and more.
+      </p>
+      <p>
+      This app was built for fellow students to track the progress of their job search.
+      <a href='/types'>Click here</a> for a detailed explanation of the job search task types.
+      Happy job searching!!!
+      </p>
+
+
+    </div>
   )
 
 }
