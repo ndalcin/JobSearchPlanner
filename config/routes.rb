094fe0e140ecc2d7a703029e1594518fb713cf25
ndalcin <dalcin.nicole@gmail.com>
2018-03-02 09:36:24 -0500

    drying up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     resources :tasks
-    resources :types
+    resources :types, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
